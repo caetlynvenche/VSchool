680e1e035d6d40c7ddc35d8b05b127a80879c8a9
Caetlyn Venche <caetlynvenche@gmail.com>
2019-08-22 09:06:10 -0600

    SillyCIA, GroceryStore, DailyPlanet

---
+++ exercises/GroceryStoreJs/app.js
@@ -0,0 +1,9 @@
+const shopper = {
+    name: "Caeltyn",
+    age: 21,
+    wearingMakeup: false,
+    groceryCart: ["Bread", "Eggs", "Almond Milk"],
+    whoIsThis: function() {
+        return shopper.name + " is " + shopper.age + " years old."
+    }
+}
